ee860fd5f12a14efe82b35fbe870b72fde98b0ee
Maris-Pidiks <maris@divikiwi.lv>
2025-07-03 16:20:21 +0300

    Start designing app

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className={"flex-1 flex-col items-center justify-center bg-gray-100"}>
+      <Text className={"text-3xl color-amber-600 mb-3 font-bold"}>Start create app from scratch!</Text>
+      <Text>with React native + Expo + NativeWind template</Text>
+    </View>
+  );
+}
