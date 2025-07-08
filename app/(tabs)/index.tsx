8840b63b0b26b2d52dec592720fb12be0ac8d8fe
Maris-Pidiks <maris@divikiwi.lv>
2025-07-08 16:19:25 +0300

    Homescreen branch first changes - bg and logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,17 @@
-import { Text, View, Image } from 'react-native'
+import { ScrollView, View, Image } from 'react-native'
 import React from 'react'
-import { globalStyles } from '../../globalStyles'
+import { images } from '../constants/images'
+import { icons } from '../constants/icons'
 
 export default function Index() {
   return (
-    <View style={globalStyles.tabscreen}>
-      <Text style={globalStyles.tabscreenText}>Index page</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute z-0 w-full h-full" />
+
+      <ScrollView className="flex-1 px-5 showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}">
+        <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5" />
+
+      </ScrollView>
     </View>
   )
 }
