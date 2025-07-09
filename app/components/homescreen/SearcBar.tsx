eb3e1c8d8a6a44f851d7b5254f1e029d8ab057d2
Maris-Pidiks <maris@divikiwi.lv>
2025-07-09 08:16:52 +0300

    add router to searchbar component navigate to serch screen

--- app/components/homescreen/SearcBar.tsx
+++ app/components/homescreen/SearcBar.tsx
@@ -2,13 +2,19 @@ import { Image, View, TextInput } from 'react-native'
 import React from 'react'
 import { icons } from '../../constants/icons'
 
-const SearchBar = () => {
+interface Props {
+    placeholder: string
+    onPress: () => void //move to the search screen
+}
+
+const SearchBar = ({ placeholder, onPress }: Props) => {
     return (
         <View className="flex-row items-center px-5 py-4 bg-dark-200">
             <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
 
-            <TextInput onPress={() => { }}
-                placeholder="Search"
+            <TextInput
+                onPress={onPress}
+                placeholder={placeholder}
                 placeholderTextColor="#ab85bb"
                 className="flex-1 ml-2 text-white"
                 value=""
